eee7c3ce50ef857a8723a969a45338120b984f32
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2021-12-31 05:30:40 +0700

    v9

--- main.js
+++ main.js
@@ -51,28 +51,28 @@ var data ={
 }
 
 
-function renderBooks(books){
-    var addBooks = document.getElementById("add-books");
-    console.log(books);
-    var htmls = books.map(function(book){
-        return`
-        <tr>
-            <th>${book.id}</th>
-            <td>${book.name}</td>
-            <td>${book.sdt}</td>
-            <td>${book.peopleNumber}</td>
-            <td>${book.day}</td>
-            <td>${book.time}</td>
-            <td>
-            <button onclick="removeBook(${book.id})">Xóa</button>
-            <button onclick="updateBook(${book.id})">Sửa</button>
-            </td>
-        </tr>
-        `
-    })
-    addBooks.innerHTML = htmls.join('');
+// function renderBooks(books){
+//     var addBooks = document.getElementById("add-books");
+//     console.log(books);
+//     var htmls = books.map(function(book){
+//         return`
+//         <tr>
+//             <th>${book.id}</th>
+//             <td>${book.name}</td>
+//             <td>${book.sdt}</td>
+//             <td>${book.peopleNumber}</td>
+//             <td>${book.day}</td>
+//             <td>${book.time}</td>
+//             <td>
+//             <button onclick="removeBook(${book.id})">Xóa</button>
+//             <button onclick="updateBook(${book.id})">Sửa</button>
+//             </td>
+//         </tr>
+//         `
+//     })
+//     addBooks.innerHTML = htmls.join('');
 
-}
+// }
 
 var listTime = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00','20:30','21:00','21:30',]
 var settime2 ='bạn chưa chọn giờ'
