6ed76205076a53b9b66a60b8807c956d88fde740
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-01-06 13:21:39 +0700

    v123

--- login-singup.js
+++ login-singup.js
@@ -1,220 +1,203 @@
-var modalLog = document.querySelector('.modal-login')
 
-function openLog(){
-    modalLog.classList.add("display-block")
-    var modal = document.querySelector('.add-content')
-    modal.innerHTML = htmlLog;
-}
+// var modalLog = document.querySelector('.modal-login')
 
-function closeModal() {
-    modalLog.classList.remove("display-block")
-}
+// function openLog(){
+//     modalLog.classList.add("display-block")
+//     var modal = document.querySelector('.add-content')
+//     modal.innerHTML = htmlLog;
+// }
 
-function createAccDone(userName,pass){
-    document.querySelector("#user-in").value = userName;
-    document.querySelector("#pass-in").value = pass;
-    document.querySelector("#tab-2").checked = false
-    document.querySelector("#tab-1").checked = true
-}
+// function closeModal() {
+//     modalLog.classList.remove("display-block")
+// }
 
-htmlLog = `<div class="login-wrap">
-<div class="login-html">
-    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-    <div class="login-form">
-        <div class="sign-in-htm">
-            <div class="group">
-                <label for="user-in" class="label">Username</label>
-                <input id="user-in" type="text" class="input">
-            </div>
-            <div class="group">
-                <label for="pass-in" class="label">Password</label>
-                <input id="pass-in" type="password" class="input" data-type="password">
-            </div>
-            <div class="group">
-                <input id="check-in" type="checkbox" class="check" checked>
-                <label for="check-in"><span class="icon"></span> Keep me Signed in</label>
-            </div>
-            <div class="group">
-                <input type="submit" class="button" onclick="signIn()" value="Sign In">
-            </div>
-            <div class="hr"></div>
-            <div class="foot-lnk">
-                <a href="#forgot">Forgot Password?</a>
-            </div>
-        </div>
-        <div class="sign-up-htm">
-            <div class="group">
-                <label for="user-up" class="label">Username</label>
-                <input id="user-up" type="text" class="input">
-            </div>
-            <div class="group">
-                <label for="pass-up" class="label">Password</label>
-                <input id="pass-up" type="password" class="input" data-type="password">
-            </div>
-            <div class="group">
-                <label for="pass-repeat-up" class="label">Repeat Password</label>
-                <input id="pass-repeat-up" type="password" class="input" data-type="password">
-            </div>
-            <div class="group">
-                <label for="email-up" class="label">Email Address</label>
-                <input id="email-up" type="text" class="input">
-            </div>
-            <div class="group">
-                <input onclick="signUp()" type="submit" class="button" value="Sign Up">
-            </div>
-            <div class="hr"></div>
-            <div class="foot-lnk">
-                <label for="tab-1">Already Member?</a>
-            </div>
-        </div>
-    </div>
-</div>
-</div>`
+// function createAccDone(userName,pass){
+//     document.querySelector("#user-in").value = userName;
+//     document.querySelector("#pass-in").value = pass;
+//     document.querySelector("#tab-2").checked = false
+//     document.querySelector("#tab-1").checked = true
+// }
 
-var usersApi = "https://61bc10bed8542f001782452a.mockapi.io/userApi"
+// htmlLog = `<div class="login-wrap">
+// <div class="login-html">
+//     <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+//     <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+//     <div class="login-form">
+//         <div class="sign-in-htm">
+//             <div class="group">
+//                 <label for="user-in" class="label">Username</label>
+//                 <input id="user-in" type="text" class="input">
+//             </div>
+//             <div class="group">
+//                 <label for="pass-in" class="label">Password</label>
+//                 <input id="pass-in" type="password" class="input" data-type="password">
+//             </div>
+//             <div class="group">
+//                 <input id="check-in" type="checkbox" class="check" checked>
+//                 <label for="check-in"><span class="icon"></span> Keep me Signed in</label>
+//             </div>
+//             <div class="group">
+//                 <input type="submit" class="button" onclick="signIn()" value="Sign In">
+//             </div>
+//             <div class="hr"></div>
+//             <div class="foot-lnk">
+//                 <a href="#forgot">Forgot Password?</a>
+//             </div>
+//         </div>
+//         <div class="sign-up-htm">
+//             <div class="group">
+//                 <label for="user-up" class="label">Username</label>
+//                 <input id="user-up" type="text" class="input">
+//             </div>
+//             <div class="group">
+//                 <label for="pass-up" class="label">Password</label>
+//                 <input id="pass-up" type="password" class="input" data-type="password">
+//             </div>
+//             <div class="group">
+//                 <label for="pass-repeat-up" class="label">Repeat Password</label>
+//                 <input id="pass-repeat-up" type="password" class="input" data-type="password">
+//             </div>
+//             <div class="group">
+//                 <label for="email-up" class="label">Email Address</label>
+//                 <input id="email-up" type="text" class="input">
+//             </div>
+//             <div class="group">
+//                 <input onclick="signUp()" type="submit" class="button" value="Sign Up">
+//             </div>
+//             <div class="hr"></div>
+//             <div class="foot-lnk">
+//                 <label for="tab-1">Already Member?</a>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+// </div>`
 
-async function signUp() {
-    var user = document.querySelector("#user-up").value
-    var pass = document.querySelector("#pass-up").value
-    var passRe = document.querySelector("#pass-repeat-up").value
-    var email = document.querySelector("#email-up").value
+// var usersApi = "https://61bc10bed8542f001782452a.mockapi.io/userApi"
 
-   if (await getName(user) === null && checkPass(pass,passRe)){
-        var data ={
-            name: user,
-            pass: pass,
-            email: email,
-            product:[]
-        };
-        createAcc(data);
-        console.log("ok")
-        createAccDone(user,pass);
+// async function signUp() {
+//     var user = document.querySelector("#user-up").value
+//     var pass = document.querySelector("#pass-up").value
+//     var passRe = document.querySelector("#pass-repeat-up").value
+//     var email = document.querySelector("#email-up").value
 
-    } else{
-        console.log("fal")
-    }    
-}
+//    if (await getName(user) === null && checkPass(pass,passRe)){
+//         var data ={
+//             name: user,
+//             pass: pass,
+//             email: email,
+//             product:[]
+//         };
+//         createAcc(data);
+//         console.log("ok")
+//         createAccDone(user,pass);
+//     } else {
+//         console.log("fal")
+//     }    
+// }
 
-async function signIn(){
-    var user = document.querySelector("#user-in").value
-    var pass = document.querySelector("#pass-in").value
+//  async function signIn(){
+//     var user = document.querySelector("#user-in").value
+//     var pass = document.querySelector("#pass-in").value
 
-    console.log(user)
-    var acc = await getName(user)
-    console.log(acc)
-    if(acc === null){
-        console.log("ten chua co")
-    } else {
-        if(checkPass(pass,acc.pass) === false){
-            console.log("pass sai")
-        } else {
-            console.log("ok")
-
-
-            var ipa = "https://61bc10bed8542f001782452a.mockapi.io/userApi/"+acc.id
-            console.log(ipa)
-            ipa(ipa)
-
-        }
-    }
-
-
-
-}
-
-
-
-async function getName(userName){
-    var listName = await getUsers()
-    
-    for(i = 0; i < listName.length; i++){
-        if (listName[i].name === userName){
-            return listName[i]
-        } 
-    }
-    return null
-    
-}
-
-function checkPass(pass,passRe){
-    if (pass === passRe){
-        return true
-    } else {
-        return false
-    }
-
-}
-
-function createAcc(data){
-    var options = {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {'Content-Type': 'application/json'}
-    }
-    handleData(usersApi,options);
-}
-
-function handleData(usersApi,options){
-    fetch(usersApi,options)
-        .then(function (response){
-            response.json()
-        })
-    
-}
-
-
-async function getUsers(){
-   return await fetch(usersApi)   
-        .then(res=>res.json())      
-}
-
-
-
-
-// var tr =checkSignIn()
-// console.log(tr)
-
-// async function c(){
-//      if (await checkSignIn()== true){
-//         return "ok"
+//     console.log(user)
+//     var acc = await getName(user)
+//     console.log(acc)
+//     if(acc === null){
+//         console.log("ten chua co")
+//     } else {
+//         if(checkPass(pass,acc.pass) === false){
+//             console.log("pass sai")
+//         } else {
+//             console.log("ok")
+//             ipa = "https://61bc10bed8542f001782452a.mockapi.io/userApi/"+acc.id;            
+//         }
 //     }
 // }
-// console.log(c())
 
-
-// async function getData(api){
-//     return await fetch(api).then(res => res.json())
+// async function getName(userName){
+//     var listName = await getUsers()
+    
+//     for(i = 0; i < listName.length; i++){
+//         if (listName[i].name === userName){
+//             return listName[i]
+//         } 
+//     }
+//     return null   
 // }
 
-// async function renderData(api){
-//     let data = await getData(api)
+// function checkPass(pass,passRe){
+//     if (pass === passRe){
+//         return true
+//     } else {
+//         return false
+//     }
 // }
 
+// function createAcc(data){
+//     var options = {
+//         method: 'POST',
+//         body: JSON.stringify(data),
+//         headers: {'Content-Type': 'application/json'}
+//     }
+//     handleData(usersApi,options);
+// }
 
-// var promise = new Promise(
-//     function(resolve,reject){
+// function handleData(usersApi,options){
+//     fetch(usersApi,options)
+//         .then(function (response){
+//             response.json()
+//         })
+// }
+
+// async function getUsers(){
+//    return await fetch(usersApi)   
+//         .then(res=>res.json())      
+// }
+
+// // var tr =checkSignIn()
+// // console.log(tr)
+
+// // async function c(){
+// //      if (await checkSignIn()== true){
+// //         return "ok"
+// //     }
+// // }
+// // console.log(c())
+
+
+// // async function getData(api){
+// //     return await fetch(api).then(res => res.json())
+// // }
+
+// // async function renderData(api){
+// //     let data = await getData(api)
+// // }
+
+
+// // var promise = new Promise(
+// //     function(resolve,reject){
         
-//         resolve(
-//             getUsers(function(us){
+// //         resolve(
+// //             getUsers(function(us){
                 
-//             })
-//         )
-//     }
-// )
+// //             })
+// //         )
+// //     }
+// // )
 
-// promise
-//     .then(function(us){
-//         console.log(us)
-//     })
+// // promise
+// //     .then(function(us){
+// //         console.log(us)
+// //     })
 
-//     .catch(function(err){
-//         console.log("b")
+// //     .catch(function(err){
+// //         console.log("b")
 
-//     })
-//     .finally(function(){
-//         console.log("x")
+// //     })
+// //     .finally(function(){
+// //         console.log("x")
 
-//     })
+// //     })
 
-// console.log(promise)
+// // console.log(promise)
--- main.js
+++ main.js
@@ -1,10 +1,244 @@
-function ipa(ipa){
-    console.log(ipa)
-    return ipa
+var modalLog = document.querySelector('.modal-login')
+var ipa = document.querySelector("#ipa")
+var modal = document.querySelector('.add-content')
+console.log(modal)
+
+
+function openLog(){
+    modalLog.classList.add("display-block")
+    var modal = document.querySelector('.add-content')
+    modal.innerHTML = htmlLog;
+}
+
+function openAcc(){
+    alert("daay ba")
+    modalLog.classList.add("display-block")
+    var modal = document.querySelector('.add-content')
+    console.log(modal)
+
+}
+
+function closeModal() {
+    modalLog.classList.remove("display-block")
+}
+
+function createAccDone(userName,pass){
+    document.querySelector("#user-in").value = userName;
+    document.querySelector("#pass-in").value = pass;
+    document.querySelector("#tab-2").checked = false
+    document.querySelector("#tab-1").checked = true
+}
+
+function signInSucc(acc){
+    document.querySelector("#btn-log").innerHTML = `
+    <a id="btn-log" onclick="openAcc()" href="#">
+    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+    ${acc.name}
+    </a>` 
+
+}
+
+htmlLog = `<div class="login-wrap">
+<div class="login-html">
+    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+    <div class="login-form">
+        <div class="sign-in-htm">
+            <div class="group">
+                <label for="user-in" class="label">Username</label>
+                <input id="user-in" type="text" class="input">
+            </div>
+            <div class="group">
+                <label for="pass-in" class="label">Password</label>
+                <input id="pass-in" type="password" class="input" data-type="password">
+            </div>
+            <div class="group">
+                <input id="check-in" type="checkbox" class="check" checked>
+                <label for="check-in"><span class="icon"></span> Keep me Signed in</label>
+            </div>
+            <div class="group">
+                <input type="submit" class="button" onclick="signIn()" value="Sign In">
+            </div>
+            <div class="hr"></div>
+            <div class="foot-lnk">
+                <a href="#forgot">Forgot Password?</a>
+            </div>
+        </div>
+        <div class="sign-up-htm">
+            <div class="group">
+                <label for="user-up" class="label">Username</label>
+                <input id="user-up" type="text" class="input">
+            </div>
+            <div class="group">
+                <label for="pass-up" class="label">Password</label>
+                <input id="pass-up" type="password" class="input" data-type="password">
+            </div>
+            <div class="group">
+                <label for="pass-repeat-up" class="label">Repeat Password</label>
+                <input id="pass-repeat-up" type="password" class="input" data-type="password">
+            </div>
+            <div class="group">
+                <label for="email-up" class="label">Email Address</label>
+                <input id="email-up" type="text" class="input">
+            </div>
+            <div class="group">
+                <input onclick="signUp()" type="submit" class="button" value="Sign Up">
+            </div>
+            <div class="hr"></div>
+            <div class="foot-lnk">
+                <label for="tab-1">Already Member?</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`
+
+var usersApi = "https://61bc10bed8542f001782452a.mockapi.io/userApi"
+
+async function signUp() {
+    var user = document.querySelector("#user-up").value
+    var pass = document.querySelector("#pass-up").value
+    var passRe = document.querySelector("#pass-repeat-up").value
+    var email = document.querySelector("#email-up").value
+
+   if (await getName(user) === null && checkPass(pass,passRe)){
+        var data ={
+            name: user,
+            pass: pass,
+            email: email,
+            product:[]
+        };
+        createAcc(data);
+        console.log("ok")
+        createAccDone(user,pass);
+    } else {
+        console.log("fal")
+    }    
+}
+
+ async function signIn(){
+    var user = document.querySelector("#user-in").value
+    var pass = document.querySelector("#pass-in").value
+
+    console.log(user)
+    var acc = await getName(user)
+    console.log(acc)
+    if(acc === null){
+        console.log("ten chua co")
+    } else {
+        if(checkPass(pass,acc.pass) === false){
+            console.log("pass sai")
+        } else {
+            console.log("ok")
+            ipa.value = "https://61bc10bed8542f001782452a.mockapi.io/userApi/"+acc.id;  
+            ipaCustomer()
+            closeModal()
+            signInSucc(acc)
+                    
+        }
+    }
+}
+
+async function getName(userName){
+    var listName = await getUsers()
+    
+    for(i = 0; i < listName.length; i++){
+        if (listName[i].name === userName){
+            return listName[i]
+        } 
+    }
+    return null   
+}
+
+function checkPass(pass,passRe){
+    if (pass === passRe){
+        return true
+    } else {
+        return false
+    }
+}
+
+function createAcc(data){
+    var options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+    }
+    handleData(usersApi,options);
+}
+
+function handleData(usersApi,options){
+    fetch(usersApi,options)
+        .then(function (response){
+            response.json()
+        })
+}
+
+async function getUsers(){
+   return await fetch(usersApi)   
+        .then(res=>res.json())      
+}
+
+// var tr =checkSignIn()
+// console.log(tr)
+
+// async function c(){
+//      if (await checkSignIn()== true){
+//         return "ok"
+//     }
+// }
+// console.log(c())
+
+
+// async function getData(api){
+//     return await fetch(api).then(res => res.json())
+// }
+
+// async function renderData(api){
+//     let data = await getData(api)
+// }
+
+
+// var promise = new Promise(
+//     function(resolve,reject){
+        
+//         resolve(
+//             getUsers(function(us){
+                
+//             })
+//         )
+//     }
+// )
+
+// promise
+//     .then(function(us){
+//         console.log(us)
+//     })
+
+//     .catch(function(err){
+//         console.log("b")
+
+//     })
+//     .finally(function(){
+//         console.log("x")
+
+//     })
+
+// console.log(promise)
+
+// ===========================================================================
+
+// var bookingApi = "https://61bc10bed8542f001782452a.mockapi.io/booking"
+var bookingApi = ""
+
+
+function ipaCustomer (){
+    bookingApi = ipa.value
+    console.log(bookingApi)
 }
 
 
-var bookingApi = "https://61bc10bed8542f001782452a.mockapi.io/booking"
+
 var timeSet = ""
 
 function start(){
@@ -40,7 +274,7 @@ function creatBook(){
         body: JSON.stringify(data),
         headers: {'Content-Type': 'application/json'}
     }
-    fetch(bookingApi+ '/' + "",options)
+    fetch(bookingApi,options)
         .then(function (response){
             response.json()
         })
